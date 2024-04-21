444331822fcb6cbcc7923af9a8afc31169f09f02
tanushk <tanushk2004@gmail.com>
2024-04-22 00:06:28 +0530

    updated

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ console.log(images.length);
 let index = 0;
 
 let banner = document.querySelector(".banner");
-// banner.style.backgroundImage = `url(${images[index]})`;
+banner.style.backgroundImage = `url(${images[index]})`;
 function changeslide(bool) {
   if (bool == true) {
     index = (index + 1) % images.length;
@@ -22,6 +22,7 @@ function changeslide(bool) {
 }
 let nextslide = document.getElementById("next_banner");
 let prevslide = document.getElementById("previous_banner");
+
 nextslide.addEventListener("click", function () {
   changeslide(true);
 });
@@ -29,3 +30,37 @@ prevslide.addEventListener("click", function () {
   changeslide(false);
 });
 //---------------------------------------------------------
+
+const protectionslides = document.querySelectorAll(".protectionimages");
+// const visibleslides = Array.from(protectionslides).slice(0, 6);
+// const hiddenSlides = Array.from(protectionslides).slice(6);
+const nextprotbutton = document.getElementById("next_product_protection");
+const prevprotbutton = document.getElementById("previous_product_protection");
+const nextseedbutton = document.getElementById("next_product_seeds");
+const prevseedbutton = document.getElementById("previous_product_seeds");
+let protimg = [
+  "protectionimage2.jpg",
+  "pesticidesimage.jpg",
+  "protectionimage2.jpg",
+  "pesticidesimage.jpg",
+  "protectionimage2.jpg",
+  "pesticidesimage.jpg",
+  "protectionimage2.jpg",
+  "pesticidesimage.jpg",
+  "protectionimage2.jpg",
+  "pesticidesimage.jpg",
+  "protectionimage2.jpg",
+  "pesticidesimage.jpg",
+];
+let lastslideindex = protectionslides.length - 1;
+nextprotbutton.addEventListener("click", () => {
+  let i = -6;
+  let imageindex = 6;
+  protectionslides[
+    lastslideindex - (i % 6)
+  ].style.backgroundImage = `url(${protimg[imageindex]})`;
+  i++;
+  imageindex++;
+});
+
+prevprotbutton.addEventListener("click", () => {});
