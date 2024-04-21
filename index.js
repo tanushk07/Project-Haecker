0ee3bb316f462d898601ae82c14d6f2e727df919
tanushk <tanushk2004@gmail.com>
2024-04-21 12:58:17 +0530

    updated

---
+++ index.js
@@ -0,0 +1,31 @@
+"use strict";
+//---------------------------------------------------------------
+let images = [];
+images.push("banner1.png");
+images.push("banner2.jpg");
+images.push("banner3.jpg");
+console.log(images);
+
+console.log(images.length);
+let index = 0;
+
+let banner = document.querySelector(".banner");
+// banner.style.backgroundImage = `url(${images[index]})`;
+function changeslide(bool) {
+  if (bool == true) {
+    index = (index + 1) % images.length;
+  } else {
+    index = (index - 1 + images.length) % images.length;
+  }
+  banner.style.backgroundImage = `url(${images[index]})`;
+  console.log(index);
+}
+let nextslide = document.getElementById("next_banner");
+let prevslide = document.getElementById("previous_banner");
+nextslide.addEventListener("click", function () {
+  changeslide(true);
+});
+prevslide.addEventListener("click", function () {
+  changeslide(false);
+});
+//---------------------------------------------------------
