c67058e0d706931a500bd0b760ccc0d85d2c6152
Sarrrr <balunisarthak10@gmail.com>
2024-04-23 00:59:49 +0530

    NAYI shuruat

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 "use strict";
 //---------------------------------------------------------------
 let images = [];
-images.push("banner1s.png");
-images.push("banner2s.jpg");
-images.push("banner3s.jpg");
-console.log(images);
-
-console.log(images.length);
+images.push("banner1.png");
+images.push("banner2.jpg");
+images.push("banner3.jpg");
 let index = 0;
 let banner = document.querySelector(".banner");
-// banner.style.backgroundImage = `url(${images[index]})`;
+banner.style.backgroundImage = `url(${images[index]})`;
 function changeslide(bool) {
   if (bool == true) {
     index = (index + 1) % images.length;
@@ -21,6 +18,7 @@ function changeslide(bool) {
 }
 let nextslide = document.getElementById("next_banner");
 let prevslide = document.getElementById("previous_banner");
+
 nextslide.addEventListener("click", function () {
   changeslide(true);
 });
@@ -28,3 +26,68 @@ prevslide.addEventListener("click", function () {
   changeslide(false);
 });
 //---------------------------------------------------------
+const nextprotbutton = document.getElementById("next_product_protection");
+const prevprotbutton = document.getElementById("previous_product_protection");
+const nextseedbutton = document.getElementById("next_product_seeds");
+const prevseedbutton = document.getElementById("previous_product_seeds");
+const protectionboxes = document.querySelectorAll(".protectionbox");
+const seedboxes = document.querySelectorAll(".seedbox");
+
+prevprotbutton.style.display = "none";
+prevseedbutton.style.display = "none";
+let counter = 1;
+nextprotbutton.addEventListener("click", () => {
+  if (counter < 7) {
+    prevprotbutton.style.display = "block";
+    protectionboxes.forEach((element) => {
+      element.style.right = `${counter * 16}vw`;
+    });
+    counter++;
+    console.log(counter);
+  }
+  if (counter == 7) {
+    nextprotbutton.style.display = "none";
+  }
+});
+prevprotbutton.addEventListener("click", () => {
+  if (counter > 1) {
+    nextprotbutton.style.display = "block";
+    counter--;
+    protectionboxes.forEach((element) => {
+      element.style.right = `${(counter - 1) * 16}vw`;
+    });
+    console.log(counter);
+  }
+  if (counter == 1) {
+    prevprotbutton.style.display = "none";
+  }
+});
+///////////////////////////////////////////////////////
+let counter2 = 1;
+nextseedbutton.addEventListener("click", () => {
+  if (counter2 < 7) {
+    prevseedbutton.style.display = "block";
+    seedboxes.forEach((element) => {
+      element.style.right = `${counter2 * 16}vw`;
+    });
+    counter2++;
+    console.log(counter);
+  }
+  if (counter2 == 7) {
+    nextseedbutton.style.display = "none";
+  }
+});
+prevseedbutton.addEventListener("click", () => {
+  if (counter2 > 1) {
+    nextseedbutton.style.display = "block";
+    counter2--;
+    seedboxes.forEach((element) => {
+      element.style.right = `${(counter2 - 1) * 16}vw`;
+    });
+
+    console.log(counter);
+  }
+  if (counter2 == 1) {
+    prevseedbutton.style.display = "none";
+  }
+});
