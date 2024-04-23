ba36536053ffd249e43f16e02e3fb788ff4dce17
tanushk <tanushk2004@gmail.com>
2024-04-23 15:40:18 +0530

    We the greatest

---
+++ app.js
@@ -0,0 +1,45 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const app = express();
+const PORT = 3000;
+const path = require("path");
+const hbs = require("hbs");
+const users = require("./models/kisan");
+const mongoose = require("mongoose");
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.set("view engine", "hbs");
+
+/*app.get("/", (req, res) => {
+  const { name } = req.query;
+  res.send(`Hey mofo ${name}`);
+});*/
+
+app.post("/", async (req, res) => {
+  /*let name = req.body.name;*/
+
+  const { pswd, name, phone, aadharcard } = req.body;
+  let ar = [name, pswd, phone, aadharcard];
+  /*res.send(pswd);*/
+  res.render("index", { data: ar });
+
+  await users.create({
+    name,
+    pswd,
+    phone,
+    aadharcard,
+  });
+  let u = await users.find({});
+  console.log(u);
+});
+/*app.get("/abt", (req, res) => {
+  res.send("Hey mofo");
+});  */
+
+mongoose.connect("mongodb://127.0.0.1:27017/KisanCart").then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port : ${PORT}`);
+  });
+});
---
+++ models/kisan.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  pswd: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  aadharcard: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("users", UserSchema); //creating the model of users with schema
